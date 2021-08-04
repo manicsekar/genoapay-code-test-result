82a0693c5ac42d1c8bf8575359eb594281659d8b
Damon O'Neill <damon.o'neill@latitudefinancial.com>
2021-08-04 13:41:30 +1200

    Initial Boilerplate Spring boot application

---
+++ src/main/java/com/latitude/genoapay/codingchallenge/controller/StockController.java
@@ -0,0 +1,11 @@
+package com.latitude.genoapay.codingchallenge.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StockController {
+
+	public Object getMaxProfit() {
+		return null;
+	}
+}
